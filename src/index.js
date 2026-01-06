68bca55ec6ee35ace126baff7bec52d06e64f522
Gufran-4518 <gufran4518@gmail.com>
2026-01-06 20:15:17 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -7,7 +7,16 @@ dotenv.config({
 })
 
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`SERVER IS RUNNING AT PORT : ${process.env.PORT}`);
+        
+    })
+})
+.catch((error)=>{
+    console.log("MONGODB CONNECTION FAILED !!!", error);
+    
+})
 
 /*
 import express from "express";
