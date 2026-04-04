644d4c8c2b4ea40861a6d848b32cb9c4c857068f
Gufran-4518 <gufran4518@gmail.com>
2026-04-04 13:03:25 +0530

    fix: update cookie options for secure and sameSite attributes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -124,11 +124,11 @@ const loginUser = asyncHandler(async (req, res) => {
     // send cookie
     const options = {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax"
-        
-        // secure: true,
-        // sameSite: "none",
+        // secure: false,
+        // sameSite: "lax"
+
+        secure: true,
+        sameSite: "none",
     }
 
     return res
