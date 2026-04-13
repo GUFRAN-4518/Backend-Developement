fe611df8acaa5afb28db7da90bc1ee30e83d2122
Gufran-4518 <gufran4518@gmail.com>
2026-04-13 16:25:56 +0530

    fix: update CORS allowed origin for clipjoy to use HTTPS

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors({
     origin: [
         "https://youtube-frontend-lime.vercel.app",
-        "http://clipjoy.vercel.app"
+        "https://clipjoy.vercel.app"
     ],
     // origin: "http://localhost:5173",
     credentials: true,
