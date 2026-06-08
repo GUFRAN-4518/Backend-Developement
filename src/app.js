004c4c8869c270e7a9e086d001e535056c06407f
Gufran-4518 <gufran4518@gmail.com>
2026-06-08 14:04:13 +0530

    fix: set trust proxy for express app configuration

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 const app = express()
+
+app.set("trust proxy", 1)
 app.use(cors({
     origin: "https://clipjoy-eight.vercel.app",
     // origin: "http://localhost:5173",
